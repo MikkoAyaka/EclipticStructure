12d281db1989b34a64afedd272ba71016f52258e
MikkoAyaka <3401286177@qq.com>
2024-02-16 21:53:38 +0800

    修正了 StructureCompanion 中无法构造 Structure 对象的问题

--- src/main/java/org/wolflink/minecraft/plugin/eclipticstructure/structure/StructureCompanion.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticstructure/structure/StructureCompanion.kt
@@ -5,9 +5,10 @@ import org.wolflink.minecraft.plugin.eclipticstructure.structure.builder.Builder
 abstract class StructureCompanion<T: Structure> {
     abstract val blueprints: List<Blueprint>
     abstract val clazz: Class<T>
+    abstract fun supplier(blueprint: Blueprint,builder: Builder): T
     fun create(structureLevel: Int, builder: Builder): T {
         val blueprint = blueprints.getOrNull(structureLevel-1)
             ?: throw IllegalArgumentException("不支持的建筑等级：${structureLevel}")
-        return clazz.getConstructor(Blueprint::class.java,Builder::class.java).newInstance(blueprint,builder)
+        return supplier(blueprint,builder)
     }
 }
