ac5cfeb6611551bf1157e59c9e3936200719f8a9
MikkoAyaka <3401286177@qq.com>
2024-02-18 14:15:17 +0800

    重命名 Logger -> ESLogger

--- src/main/java/org/wolflink/minecraft/plugin/eclipticstructure/structure/registry/StructureRegistry.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticstructure/structure/registry/StructureRegistry.kt
@@ -1,7 +1,7 @@
 package org.wolflink.minecraft.plugin.eclipticstructure.structure.registry
 
 import org.bukkit.plugin.Plugin
-import org.wolflink.minecraft.plugin.eclipticstructure.logger
+import org.wolflink.minecraft.plugin.eclipticstructure.esLogger
 import org.wolflink.minecraft.plugin.eclipticstructure.structure.blueprint.Blueprint
 import org.wolflink.minecraft.plugin.eclipticstructure.structure.Structure
 import org.wolflink.minecraft.plugin.eclipticstructure.structure.builder.Builder
@@ -14,7 +14,7 @@ object StructureRegistry {
     fun register(plugin: Plugin, structureTypeName: String, blueprints: List<Blueprint>, structureSupplier:(Int, Builder)-> Structure) {
         val structureRegistryItem = StructureRegistryItem(plugin,structureTypeName,blueprints,structureSupplier)
         if(contains(structureTypeName)) {
-            logger.warning("$structureTypeName 该建筑结构名已经被 ${plugin.name} 插件注册了，无法重复注册，请更改结构名称")
+            esLogger.warning("$structureTypeName 该建筑结构名已经被 ${plugin.name} 插件注册了，无法重复注册，请更改结构名称")
         } else structureRegistryItems.add(structureRegistryItem)
     }
     fun unregister(structureTypeName: String) {
@@ -26,7 +26,7 @@ object StructureRegistry {
             }
         }
         if(structureRegistryItem == null) {
-            logger.warning("$structureTypeName 建筑结构不存在于注册列表中，无法取消注册")
+            esLogger.warning("$structureTypeName 建筑结构不存在于注册列表中，无法取消注册")
         } else structureRegistryItems.remove(structureRegistryItem)
     }
     fun get(structureTypeName: String) = structureRegistryItems.firstOrNull { it.structureTypeName == structureTypeName } ?: throw NullPointerException("$structureTypeName 未被注册")
