05ff8134b26fcee05be8ae96d168af43e8489001
MikkoAyaka <3401286177@qq.com>
2024-02-22 12:52:33 +0800

    删除不必要的属性

--- src/main/java/org/wolflink/minecraft/plugin/eclipticstructure/structure/StructureCompanion.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticstructure/structure/StructureCompanion.kt
@@ -5,7 +5,6 @@ import org.wolflink.minecraft.plugin.eclipticstructure.structure.builder.Builder
 
 abstract class StructureCompanion<T: Structure> {
     abstract val blueprints: List<Blueprint>
-    abstract val clazz: Class<T>
     abstract fun supplier(blueprint: Blueprint, builder: Builder): T
     fun create(structureLevel: Int, builder: Builder): T {
         val blueprint = blueprints.getOrNull(structureLevel-1)
