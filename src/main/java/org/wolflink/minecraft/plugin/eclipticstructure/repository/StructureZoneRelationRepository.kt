0a7924f6635cf4c8097088ab1536bb8191ecf4cf
MikkoAyaka <3401286177@qq.com>
2024-02-22 14:48:35 +0800

    补充空安全类型

--- src/main/java/org/wolflink/minecraft/plugin/eclipticstructure/repository/StructureZoneRelationRepository.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticstructure/repository/StructureZoneRelationRepository.kt
@@ -3,13 +3,12 @@ package org.wolflink.minecraft.plugin.eclipticstructure.repository
 import org.wolflink.minecraft.plugin.eclipticstructure.structure.Structure
 import org.wolflink.minecraft.plugin.eclipticstructure.structure.Zone
 import org.wolflink.minecraft.wolfird.framework.database.repository.RelationRepository
+import javax.annotation.Nullable
 
 object StructureZoneRelationRepository: RelationRepository<Int, Structure,Int,Zone>() {
     override fun getPrimaryKey1(value: Structure) = value.id
-
-    override fun getValue1(key: Int): Structure = StructureRepository.find(key)
-
-    override fun getValue2(key: Int): Zone = ZoneRepository.find(key)
+    override fun getValue1(key: Int): Structure? = StructureRepository.find(key)
+    override fun getValue2(key: Int): Zone? = ZoneRepository.find(key)
 
     override fun getPrimaryKey2(value: Zone) = value.id
 }
