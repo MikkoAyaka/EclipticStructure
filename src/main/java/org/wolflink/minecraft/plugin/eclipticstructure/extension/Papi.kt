5d95ecb5b0121120bde79abab778ce4fdf5dd076
MikkoAyaka <3401286177@qq.com>
2024-02-26 21:50:35 +0800

    完善变量封装

--- src/main/java/org/wolflink/minecraft/plugin/eclipticstructure/extension/Papi.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticstructure/extension/Papi.kt
@@ -1,6 +1,7 @@
 package org.wolflink.minecraft.plugin.eclipticstructure.extension
 
 import me.clip.placeholderapi.PlaceholderAPI
+import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 
-fun String.parsePapi(player: Player) = PlaceholderAPI.setPlaceholders(player,this)
+fun String.parsePapi(player: OfflinePlayer) = PlaceholderAPI.setPlaceholders(player,this)
